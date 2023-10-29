0df5da541a847e4593a6fd63ef01617b1634a46b
tamashiro-syuta <syuta5825@gmail.com>
2023-10-29 17:43:19 +0900

    ✨ fontをnext/fontsを使って最適化

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* NOTE <body>要素にInterを追加することで、アプリケーション全体にフォントが適用される */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
