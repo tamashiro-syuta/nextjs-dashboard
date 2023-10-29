052c5aff660f2ec363e92a81860d598de26e49e2
tamashiro-syuta <syuta5825@gmail.com>
2023-10-29 17:16:40 +0900

    💄 CSS Modulesを使ったスタイルの当て方を追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import styles from './ui/home.module.css';
 import AcmeLogo from '@/app/ui/acme-logo';
 
 import Link from 'next/link';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+
+      {/* NOTE ----- CSS modulesを使ったスタイルの当て方 ----- */}
+      <div className={styles.shape}></div>
+      {/* NOTE ----------------------------------------- */}
+
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
