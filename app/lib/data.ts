b54dd531deb0799c91bc54746e3cd0acebdf4bab
tamashiro-syuta <syuta5825@gmail.com>
2023-10-29 19:51:59 +0900

    ✨ RSCを使ってセキュアにDBにクエリを投げてデータをfetch

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -64,6 +64,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
+    // NOTE Promise.all()とすることで、3つのクエリが並列で実行できる
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
